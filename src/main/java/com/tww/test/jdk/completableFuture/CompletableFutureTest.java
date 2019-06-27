591683e36c054b813f1aa87983f8d89ef381bc59
Tangwenwei <tangwenwei@haixue.com>
2019-06-27 18:27:43 +0800

    CompletableFuture

--- src/main/java/com/tww/test/jdk/completableFuture/CompletableFutureTest.java
+++ src/main/java/com/tww/test/jdk/completableFuture/CompletableFutureTest.java
@@ -1,6 +1,10 @@
 package com.tww.test.jdk.completableFuture;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class CompletableFutureTest {
     public static void main(String[] args) {
@@ -8,7 +12,91 @@ public class CompletableFutureTest {
     }
 
     private static void test1() {
-        CompletableFuture<String> cf = CompletableFuture.completedFuture("complete");
-        CompletableFuture future = cf.thenApply(String::toUpperCase);
+        List<Integer> ids = new ArrayList<>();
+        ids.add(5);
+        ids.add(8);
+        ids.add(10);
+        ids.add(13);
+        CompletableFuture<List<User>> future = CompletableFuture.completedFuture(ids).thenCompose(list -> {
+            List<CompletableFuture<User>> userFutureList = list.stream().map(id -> {
+                CompletableFuture<String> nameFuture = CompletableFuture.supplyAsync(() -> getName(id));
+                CompletableFuture<Integer> stateFuture = CompletableFuture.supplyAsync(() -> getState(id));
+                return nameFuture.thenCombineAsync(stateFuture, (name, state) -> {
+                    User user = new User();
+                    user.setId(id);
+                    user.setName(name);
+                    user.setState(state);
+                    return user;
+                });
+            }).collect(Collectors.toList());
+            CompletableFuture<User>[] completableFuture =
+                    userFutureList.toArray(new CompletableFuture[userFutureList.size()]);
+            CompletableFuture<Void> allDone = CompletableFuture.allOf(completableFuture);
+            return allDone.thenApply(
+                    all -> userFutureList.stream().map(CompletableFuture::join).collect(Collectors.toList()));
+        });
+        List<User> users = future.join();
+        System.out.println(users);
+    }
+
+    private static String getName(Integer id) {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "name" + id;
+    }
+
+    private static Integer getState(Integer id) {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return 0 - id;
+    }
+
+
+}
+
+
+class User {
+    private Integer id;
+    private String name;
+    private Integer state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", state=").append(state);
+        sb.append('}');
+        return sb.toString();
     }
 }
