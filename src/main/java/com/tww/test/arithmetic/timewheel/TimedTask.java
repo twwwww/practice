e34cc2dd482df4c87f9b02b65d810c0e4c2de293
tangwenwei <tangwenwei@haixue.com>
2019-07-29 03:27:44 +0800

    timewheel

---
+++ src/main/java/com/tww/test/arithmetic/timewheel/TimedTask.java
@@ -0,0 +1,66 @@
+package com.tww.test.arithmetic.timewheel;
+
+/**
+ * Created by Anur IjuoKaruKas on 2018/10/16
+ *
+ * 需要延迟执行的任务，放在槽 {@link Bucket} 里面
+ */
+public class TimedTask {
+
+    private Class<?> fromClazz;
+
+    /** 延迟多久执行时间 */
+    private long delayMs;
+
+    /** 过期时间戳 */
+    private long expireTimestamp;
+
+    /** 任务 */
+    private Runnable task;
+
+    /** 是否取消 */
+    private volatile boolean cancle;
+
+    protected Bucket bucket;
+
+    protected TimedTask next;
+
+    protected TimedTask pre;
+
+    public String desc;
+
+    public TimedTask(long delayMs, Runnable task) {
+        this.delayMs = delayMs;
+        this.task = task;
+        this.bucket = null;
+        this.next = null;
+        this.pre = null;
+        this.expireTimestamp = System.currentTimeMillis() + delayMs;
+        this.cancle = false;
+    }
+
+    public void cancle() {
+        cancle = true;
+    }
+
+    public boolean isCancle() {
+        return cancle;
+    }
+
+    public Runnable getTask() {
+        return task;
+    }
+
+    public long getDelayMs() {
+        return delayMs;
+    }
+
+    public long getExpireTimestamp() {
+        return expireTimestamp;
+    }
+
+    @Override
+    public String toString() {
+        return desc;
+    }
+}
