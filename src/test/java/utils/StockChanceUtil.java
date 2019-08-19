78b8b48897cd21b006828b42e8ced7b898624be5
Tangwenwei <tangwenwei@haixue.com>
2019-08-19 18:15:11 +0800

    timeWheel

--- src/test/java/utils/StockChanceUtil.java
+++ src/test/java/utils/StockChanceUtil.java
@@ -16,12 +16,10 @@ public class StockChanceUtil {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        List<EsHit> esHits = parseJsonStr();
-        List<String> chanceIds = esHits.stream().map(EsHit::get_id).collect(Collectors.toList());
-        System.out.println(chanceIds);
+        parseAggsJsonStr();
     }
 
-    private static List<EsHit> parseJsonStr() throws Exception {
+    private static void parseJsonStr() throws Exception {
         StringBuilder sb = new StringBuilder();
         File file = new File("src/chances.txt");
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -34,6 +32,28 @@ public class StockChanceUtil {
             }
         }
         List<EsHit> esHits = JSONObject.parseArray(sb.toString(), EsHit.class);
-        return esHits;
+        List<Long> chanceIds = esHits.stream()
+                .map(ch -> ch.get_source().getId())
+                .collect(Collectors.toList());
+        System.out.println(chanceIds);
+    }
+
+    private static void parseAggsJsonStr() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        File file = new File("src/chances.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String tempString;
+            while ((tempString = reader.readLine()) != null) {
+                if ("".equals(tempString)) {
+                    continue;
+                }
+                sb.append(tempString);
+            }
+        }
+        List<AggsEntity> esHits = JSONObject.parseArray(sb.toString(), AggsEntity.class);
+        List<String> chanceIds = esHits.stream()
+                .map(AggsEntity::getKey)
+                .collect(Collectors.toList());
+        System.out.println(chanceIds);
     }
 }
