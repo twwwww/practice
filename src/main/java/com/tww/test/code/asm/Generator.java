3303becc41e8194a94ed4c87152c163d18005a5b
tangwenwei <tangwenwei@haixue.com>
2021-06-04 09:16:29 +0800

    asm javassist

---
+++ src/main/java/com/tww/test/code/asm/Generator.java
@@ -0,0 +1,26 @@
+package com.tww.test.code.asm;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class Generator {
+    public static void main(String[] args) throws Exception {
+        //读取
+        ClassReader classReader = new ClassReader("./asm/Base");
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        //处理
+        ClassVisitor classVisitor = new MyClassVisitor(classWriter);
+        classReader.accept(classVisitor, ClassReader.SKIP_DEBUG);
+        byte[] data = classWriter.toByteArray();
+        //输出
+        File f = new File("practice/target/classes/com/tww/test/code/asm/Base.class");
+        FileOutputStream fout = new FileOutputStream(f);
+        fout.write(data);
+        fout.close();
+        System.out.println("now generator cc success!!!!!");
+    }
+}
