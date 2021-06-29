a2a432eb76a5832a86005d944dec95611ac48208
tangwenwei <tangwenwei@haixue.com>
2021-06-29 16:11:26 +0800

    1q3

--- src/main/java/com/tww/test/Main.java
+++ src/main/java/com/tww/test/Main.java
@@ -1,79 +1,53 @@
 package com.tww.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        //candy(new int[]{1,0,2});
-        //cuttingRope(6);
-        cherryPickup(new int[][]{new int[]{0,1,-1},new int[]{1,0,-1},new int[]{1,1,1}});
+        //maxSatisfied(new int[]{1,0,1,2,1,1,7,5},new int[]{0,1,0,1,0,1,0,1},3);
+        removeKdigits("10001",4);
+        //cherryPickup(new int[][]{new int[]{0,1,-1},new int[]{1,0,-1},new int[]{1,1,1}});
     }
 
-    public static int cherryPickup(int[][] grid) {
-
-        int[] nums = new int[];
-        Map<Integer, Long> mapper = Arrays.stream(nums)
-                .sorted()
-                .boxed()
-                .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
-        int[][] newNums = mapper.entrySet().stream().sorted(Map.Entry.comparingByKey())
-                .map(item -> new int[]{item.getKey(), item.getValue().intValue()}).toArray(int[][]::new);
-
-        int m = grid.length;
-        if (m == 1) {
-            return grid[0][0];
-        }
-        int[][][] dp = new int[m][m][2];
-
-        boolean is1 = grid[0][0] == 1;
-        if (!is1) {
-            grid[0][0] = 1;
+    public static String removeKdigits(String num, int k) {
+        List<Integer> list = Arrays.stream(num.split("")).map(Integer::new).collect(Collectors.toList());
+        LinkedList<Integer> stack = new LinkedList<>();
+        List<Integer> removeIndex = new ArrayList<>();
+        stack.push(0);
+        a : for (int i = 1; i < list.size(); i++) {
+            while (!stack.isEmpty() && list.get(stack.peek()) > list.get(i)) {
+                Integer pop = stack.pop();
+                removeIndex.add(pop);
+                if (removeIndex.size() >= k) {
+                    break a;
+                }
+            }
+            stack.push(i);
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j][0] = grid[i][j];
-                    dp[i][j][1] = grid[i][j];
-                } else if (grid[i][j] == -1) {
-                    dp[i][j][0] = 0;
-                    dp[i][j][1] = 0;
-                } else if (i == 0) {
-                    dp[i][j][0] = dp[i][j - 1][0] > 0 ? dp[i][j - 1][0] + grid[i][j] : 0;
-                    dp[i][j][1] = dp[i][j][0];
-                } else if (j == 0) {
-                    dp[i][j][0] = dp[i - 1][j][0] > 0 ? dp[i - 1][j][0] + grid[i][j] : 0;
-                    dp[i][j][1] = dp[i][j][0];
-                } else {
-                    int dp0 = Integer.max(dp[i - 1][j][0], dp[i][j - 1][0]);
-                    dp[i][j][0] = dp0 > 0 ? dp0 + grid[i][j] : 0;
-                    int dp1 = Integer.max(dp[i - 1][j][0] + dp[i][j - 1][0], Integer.max(dp[i - 1][j][1], dp[i][j - 1][1]));
-                    dp[i][j][1] = dp1 > 0 ? dp1 + grid[i][j] : 0;
+        if (removeIndex.size() < k) {
+            for (int i = list.size() - 1; i >= list.size() - 1 - k + removeIndex.size() + 1; i++) {
+                removeIndex.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean zero = true;
+        for (int i = 0; i < list.size() && sb.length() < num.length() - k; i++) {
+            if (!removeIndex.contains(i)) {
+                if (list.get(i) != 0 || !zero) {
+                    sb.append(list.get(i));
+                    zero = false;
                 }
             }
         }
-        int result = dp[m - 1][m - 1][1] <= 0 || is1 ? dp[m - 1][m - 1][1] : dp[m - 1][m - 1][1] - 2;
-        return result;
 
-    }
-
-    private int binarySearchFirstGt(int[] arr,int findNum) {
-
-        int length = arr.length;
-        int left = 0;
-        int right = length - 1;
-        while(left <= right) {
-            int mid = (left+right) / 2;
-            if(arr[mid] > findNum){
-                right = mid-1;
-            } else if (arr[mid] <= findNum) {
-                left = mid+1;
-            }
-        }
-        return left;
-
+        return sb.length() > 0 ? sb.toString() : "0";
     }
 
 }
